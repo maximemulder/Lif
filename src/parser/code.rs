010deaea50cb09ce1c7f2979c5d48e459c68ba60
Maxime Mulder <maxime.mulder@viacesi.fr>
2021-06-17 01:15:26 +0200

    remove some debug code

--- src/parser/code.rs
+++ src/parser/code.rs
@@ -23,11 +23,9 @@ impl Code {
         });
 
         let syntax_tree = grammar.parse(production, code.get_ref()).unwrap();
-        crate::parser::printer::tree(&syntax_tree);
         code.syntax_tree = Some(syntax_tree);
         code.walk_tree = Some(program(Ref::new(code.syntax_tree.as_ref().unwrap())));
         code
-
     }
 
     pub fn from_file(grammar: &Grammar, production: ArenaRef<dyn Descent>, program: &dyn Fn(Ref<SNode>) -> WNode, name: &str) -> Option<Own<Self>> {
